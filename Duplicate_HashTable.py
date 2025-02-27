749d5379a32f852a9ec584866ea88f9c9d9b53f4
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-02-27 12:09:14 +0530

    Add files via upload

---
+++ Duplicate_HashTable.py
@@ -0,0 +1,16 @@
+def find_duplicates(nums):
+    num_counts = dict()
+    for num in nums:
+        num_counts[num] = num_counts.get(num, 0) + 1
+    return [num for num, count in num_counts.items() if count > 1]
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
